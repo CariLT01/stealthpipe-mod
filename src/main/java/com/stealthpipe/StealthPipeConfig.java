8de0c8613a0a1267d6d5216fad9f5374d053d6fc
= <publicuser0999847695359@gmail.com>
2026-02-21 19:42:01 -0500

    Bump version

--- src/main/java/com/stealthpipe/StealthPipeConfig.java
+++ src/main/java/com/stealthpipe/StealthPipeConfig.java
@@ -19,7 +19,7 @@ public class StealthPipeConfig {
 
     public String CONNECTION_SUFFIX = ".stealth.link";
     public String MOD_VERSION = "4.0.0";
-    public String REAL_MOD_VERSION = "5.0.3";
+    public String REAL_MOD_VERSION = "5.1.0";
 
     public boolean ENABLE_BATCHED_PACKETS = DefaultConfigValues.ENABLE_BATCHED_PACKETS;
     public int PACKET_BATCHING_INTERVAL_MS = DefaultConfigValues.PACKET_BATCHING_INTERVAL_MS;
