ff3ce590a11703c8d19241ed6c9aeba9ca046c71
= <publicuser0999847695359@gmail.com>
2026-03-10 19:42:21 -0400

    Set connect to PROD

--- src/main/java/com/stealthpipe/DefaultConfigValues.java
+++ src/main/java/com/stealthpipe/DefaultConfigValues.java
@@ -2,7 +2,7 @@ package com.stealthpipe;
 
 public class DefaultConfigValues {
 
-    public static final String RELAY_IP = "https://mcpipeservice-go-staging.onrender.com";
+    public static final String RELAY_IP = "https://mcpipeservice-go.onrender.com";
     public static final int RELAY_PING_ATTEMPTS = 5;
     public static final boolean ONLINE_MODE = true;
     public static final boolean ENABLE_BATCHED_PACKETS = true;
