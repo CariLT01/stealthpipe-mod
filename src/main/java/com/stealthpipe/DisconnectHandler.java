71d9f348769a0d3058daa01c1e432433a96e6ff6
= <publicuser0999847695359@gmail.com>
2026-03-14 00:50:53 -0400

    add error message translations, better disconnect messages

--- src/main/java/com/stealthpipe/DisconnectHandler.java
+++ src/main/java/com/stealthpipe/DisconnectHandler.java
@@ -11,12 +11,17 @@ public class DisconnectHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StealthPipe.MOD_ID);
 
+
+
+
     public static void showDisconnectMessageAndRetry(String reason) {
         String realReason = Objects.equals(reason, "") ? "None provided" : reason;
+        String reasonPresented = ErrorMessages.errorReasonToMessage(reason);
+
 
         LOGGER.info("attempt retry");
         ModState.minecraftServer.get().getPlayerList().broadcastSystemMessage(
-                Component.literal(String.format("§8[StealthPipe§8] : §cSignaling connection to relay disconnected. Attempting to reconnect... (error code: %s)", realReason)).withStyle(ChatFormatting.RED),
+                Component.literal(String.format("§8[StealthPipe§8] : §cSignaling connection to relay disconnected.\n%s\n§7Attempting to reconnect... (error code: %s)", reasonPresented, realReason)).withStyle(ChatFormatting.RED),
                 false
         );
 
@@ -25,14 +30,16 @@ public class DisconnectHandler {
 
     public static void showClientDisconnectMessage(boolean gotMessages, String reason) {
         String realReason = !Objects.equals(reason, "") ? reason : "None provided";
+        String reasonPresented = ErrorMessages.errorReasonToMessage(reason);
+
 
         if (StealthPipe.CLIENT_PROXY != null) {
             if (gotMessages) {
                 StealthPipe.CLIENT_PROXY.disconnectWithReason(
-                        String.format("§cStealthPipe connection disconnected.\nThe host may have closed the room. If not, try reconnecting.\n(error code: %s)", realReason), 250);
+                        String.format("§cStealthPipe connection disconnected.\n\n§7%s\n§8(error code: %s)", reasonPresented, realReason), 250);
             } else {
                 StealthPipe.CLIENT_PROXY.disconnectWithReason(
-                        String.format("§cStealthPipe failed to connect.\nCheck room code and try again.\n\nMake sure you are using the latest client version.\n(error code: %s)", realReason), 0);
+                        String.format("§cStealthPipe failed to connect.\n\n§7%s\n§8(error code: %s)", reasonPresented, realReason), 0);
             }
         }
     }
---
+++ src/main/java/com/stealthpipe/ErrorMessages.java
@@ -0,0 +1,39 @@
+package com.stealthpipe;
+
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class ErrorMessages {
+    public static final Map<String, String> MESSAGES = Map.ofEntries(
+            entry("HOST_DISCONNECTED", "The host has disconnected."),
+            entry("HIGH_BANDWIDTH", "You are sending too much data. Try lowering your render distance and try again."),
+            entry("HIGH_USAGE", "You are sending too much data. Please wait and try again."),
+            entry("WSS_READ_FAILED", "Socket read failed. Please try reconnecting."),
+            entry("WSS_WRITE_FAILED", "Socket write failed. Please try reconnecting."),
+            entry("WSS_IDLE", "Your room has been empty for too long."),
+            entry("ROOM_NOT_FOUND", "Room does not exist. Check room code and try again."),
+            entry("ROOM_NO_HOST", "You have connected before the host has. Please wait and try again."),
+            entry("ROOM_BAD_STATE", "The room is not in a state that allows joining."),
+            entry("INTERNAL_ERROR", "An unexpected error occurred. Please try again."),
+            entry("INVALID_REQUEST_ID", "Couldn't correlate the joining connection with an existing connection. Please try again."),
+            entry("PAIR_LINK_FAILED", "Couldn't pair the client and host connections. Please try again."),
+            entry("INVALID_ACTION", "This action is forbidden."),
+            entry("REUSE_TOKEN_PRESENTED", "Another client has reused your room code. Please restart the session to generate a new code."),
+            entry("UNKNOWN", "An unknown error occurred. Please try again."),
+            entry("CLIENT_DISCONNECTED", "The client has disconnected."),
+            entry("UNSPECIFIED", "Disconnect reason has not been specified"),
+            entry("PACKET_TOO_LARGE", "A packet you sent was too large."),
+            entry("SIGNAL_DISCONNECTED", "The main room connection has disconnected."),
+            entry("BAD_REQUEST", "The client has sent an unexpected request."),
+            entry("WRTC_DISCONNECTED", "The connection to the other client has disconnected."),
+            entry("ABNORMAL_DISCONNECT", "Disconnected unexpectedly. Please check your Internet connection and try again."),
+            entry("INVALID_VERSION", "This version of the mod is invalid!"),
+            entry("OUTDATED_CLIENT", "Outdated client. Please update to the latest version of StealthPipe."),
+            entry("UNSUPPORTED_CLIENT", "Unsupported client version. Your client is too new for this relay.")
+    );
+    public static final String DEFAULT_MESSAGE = "Disconnected.";
+
+    public static String errorReasonToMessage(String reason) {
+        return MESSAGES.getOrDefault(reason, DEFAULT_MESSAGE);
+    }
+}
